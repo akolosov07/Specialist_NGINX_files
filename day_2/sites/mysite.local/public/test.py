af436e06d7e8b76b0c23087d167bf8d1377a2a16
Alexander <avt757@hotmail.com>
2022-05-21 22:11:56 +0300

    day_2

---
+++ day_2/sites/mysite.local/public/test.py
@@ -0,0 +1,8 @@
+#!C:\Users\ASUS\AppData\Local\Programs\Python\Python310\python.exe
+
+#import sys, io
+#sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+
+
+print ("Content-Type: text/html; charset='utf-8'\n\n")
+print ("Привет, мир! Hello, world!")
